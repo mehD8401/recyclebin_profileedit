fa8cb5fe6e859400ff172d8a9b0ae9a50b473043
mehD8401 <mhassan171205@bscse.uiu.ac.bd>
2021-03-27 09:34:32 +0600

    edit_profile_form

---
+++ recycle_bin/browse_file/user/edit_profile.php
@@ -0,0 +1,28 @@
+<br>
+<br>
+
+<form action="../../api/post/api_edit_profile.php" method="post">
+
+ <label >email: </label>
+  <input type="text" name="email"><br><br>
+
+  <label >name: </label>
+  <input type="text" name="name"><br><br>
+  
+  <label >Description: </label>
+  <textarea name="description" cols="50" rows="10"></textarea><br><br>
+  
+  <label >Address: </label>
+  <textarea name="address" cols="50" rows="3"></textarea><br><br>
+
+  <label >phone: </label>
+  <input type="text"  name="phone"><br><br>
+  
+  <label >photo: </label>
+  <input type="file" name="profile_pht" ><br><br>
+  
+  <input type="submit" >
+</form>
+
+<br>
+<br>
